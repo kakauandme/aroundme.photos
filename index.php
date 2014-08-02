971f70630d0c032c6ffb1b0b314663fedbe7ba0b
Kirill Kliavin <kirirll@studiothick.com>
2014-08-02 15:27:58 +1000

    copy

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+
+$title = 'Around me photos';
+$subtitle = 'Explore social activity next to you';
+$description = 'Checkout real-time updates from Instagram around your location on a map and discover what is happening in your area';
+
+?>
 <!DOCTYPE html>
 <!--[if lt IE 9]><html class="no-js ie"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
@@ -6,11 +13,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 
-	<title>Around me photos | Discover pictures next to you</title>
+	<title><?php echo $title . " | " . $subtitle; ?></title>
 
 	<!-- SEO -->
-	<meta name="description" content="View pictures from Instagram posted around your location on a map and explore what is happening next to you.">
-	<meta name="keywords" content="map, Instagram, location, photos, pictures, images">
+	<meta name="description" content="<?php echo $description; ?>">
+	<!-- <meta name="keywords" content="map, Instagram, location, photos, pictures, images"> -->
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="/css/header.css">
@@ -28,60 +35,12 @@ html{height:100%;-webkit-text-size-adjust:100%}body{height:100%;margin:0}#c{widt
 	<!--browsers -->
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
-
 	<!-- iOS -->
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-	<!-- Startup images --> 
-    <!-- iOS 6 & 7 iPad (retina, portrait) -->
-    <link href="/static/images/apple-touch-startup-image-1536x2008.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-             and (orientation: portrait)
-             and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 & 7 iPad (retina, landscape) -->
-    <link href="/static/images/apple-touch-startup-image-1496x2048.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-             and (orientation: landscape)
-             and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 iPad (portrait) -->
-    <link href="/static/images/apple-touch-startup-image-768x1004.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-             and (orientation: portrait)
-             and (-webkit-device-pixel-ratio: 1)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 iPad (landscape) -->
-    <link href="/static/images/apple-touch-startup-image-748x1024.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-             and (orientation: landscape)
-             and (-webkit-device-pixel-ratio: 1)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 & 7 iPhone 5 -->
-    <link href="/static/images/apple-touch-startup-image-640x1096.png"
-          media="(device-width: 320px) and (device-height: 568px)
-             and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 & 7 iPhone (retina) -->
-    <link href="/static/images/apple-touch-startup-image-640x920.png"
-          media="(device-width: 320px) and (device-height: 480px)
-             and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 iPhone -->
-    <link href="/static/images/apple-touch-startup-image-320x460.png"
-          media="(device-width: 320px) and (device-height: 480px)
-             and (-webkit-device-pixel-ratio: 1)"
-          rel="apple-touch-startup-image">
-
 	<meta name="apple-mobile-web-app-capable" content="yes">
 
-	<meta name="apple-mobile-web-app-title" content="Around me photos">
+	<meta name="apple-mobile-web-app-title" content="<?php echo $title; ?>">
 
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
@@ -90,7 +49,7 @@ html{height:100%;-webkit-text-size-adjust:100%}body{height:100%;margin:0}#c{widt
 	<link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196">
 
 	<!-- MS Tiles -->
-	<meta name="application-name" content="Around me photos"/>
+	<meta name="application-name" content="<?php echo $title; ?>"/>
 	<meta name="msapplication-config" content="browserconfig.xml" />
 	<meta name="msapplication-TileColor" content="#1a75cf">
 	<meta name="msapplication-TileImage" content="/mstile-144x144.png">
@@ -100,43 +59,27 @@ html{height:100%;-webkit-text-size-adjust:100%}body{height:100%;margin:0}#c{widt
 	<!-- https://dev.twitter.com/docs/cards/large-image-summary-card -->
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:creator" content="@kakauandme">
-	<meta name="twitter:title" content="Title should be concise and will be truncated at 70 characters.">
-	<meta name="twitter:description" content="A description that concisely summarizes the content of the page, as appropriate for presentation within a Tweet. Do not re-use the title text as the description, or use this field to describe the general services provided by the website. Description text will be truncated at the word to 200 characters.">
-	<!-- URL to a unique image representing the content of the page. Do not use a generic image such as your website logo, author photo, or other image that spans multiple pages. Images for this Card should be at least 280px in width, and at least 150px in height. Image must be less than 1MB in size. -->
-	<meta name="twitter:image:src" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg">
+	<meta name="twitter:title" content="<?php echo $title . " - " . $subtitle; ?>">
+	<meta name="twitter:description" content="<?php echo $description; ?>">
+	<meta name="twitter:image:src" content="/img/screenshot_sml.png">
 
 	<!-- Facebook OG -->
 	<!-- https://developers.facebook.com/docs/opengraph/howtos/maximizing-distribution-media-content -->
-
-	<meta property="og:title" content="Workday Sets Price Range for I.P.O." />
-	<meta property="og:site_name" content="My Favorite News"/>
-	<meta property="og:url" content="http://www.myfavnews.com/2013/1/1/workday-price-range" />
-	<meta property="og:description" content="Workday, a provider of cloud-based" />
+	<meta property="og:title" content="<?php echo $subtitle; ?>" />
+	<meta property="og:site_name" content="<?php echo $title; ?>"/>
+	<meta property="og:url" content="http://aroundme.photos/" />
+	<meta property="og:description" content="<?php echo $description; ?>" />
 	<meta property="fb:app_id" content="587080064742659" />
 	<meta property="og:type" content="website" />
 	<meta property="article:author" content="https://www.facebook.com/kirill.kliavin" />
-	<!-- The URL of an image which is used in stories published about this object. We suggest that you give us an image of at least 600x315 pixels. However, bigger is better, so if you have a 1200x630 or larger image that you can use, please give it to us. Also, we recommend that you keep images as close to a 1.91:1 aspect ratio as possible to avoid cropping. (Note: image sizes must be no more than 5MB in size.) For games, images should be square and at least 600x600 pixels. Note that you can include more than that one og:image in your object if you have more than one resolution available.-->
-	<meta property="og:image" content="https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/851565_496755187057665_544240989_n.jpg" /> 
+	<meta property="og:image" content="/img/screenshot_lrg.png" /> 
 
 
 	<!-- Google+ Schema.org microdata	 -->
-	<!--
-	<body itemscope itemtype="http://schema.org/Product">
-	<h1 itemprop="name">Shiny Trinket</h1>
-	<img itemprop="image" src="{image-url}" />
-	 <p itemprop="description">Shiny trinkets are shiny.</p>
-	</body>
-	-->
-	<!-- Update your html tag to include the itemscope and itemtype attributes. -->
-	<html itemscope itemtype="http://schema.org/Website">
-
-	<!-- Add the following three tags inside head. -->
-	<meta itemprop="name" content="Around me photos">
-	<meta itemprop="description" content="Description">
-	<meta itemprop="image" content="{image-url}">
+	<meta itemprop="image" content="/img/screenshot_lrg.png">
 
 </head>
-<body id="body" class="dev">
+<body id="body" itemscope itemtype="http://schema.org/Website">
 	<div id="container">
 		<div id="pusher">
 			<div id="c">
@@ -150,20 +93,20 @@ html{height:100%;-webkit-text-size-adjust:100%}body{height:100%;margin:0}#c{widt
 				<span>
 		    		<span id="social">
 						<p>
-							<a href="https://twitter.com/share?url=http://aroundme.photos/&hashtags=aroundmephotos" target="_blank" title="Tweet">
-								<?php include("img/iconmonstr-twitter-4-icon.svg"); ?>
+							<a href="https://twitter.com/share?url=http://aroundme.photos/&hashtags=aroundmephotos&text=<?php echo $subtitle; ?>" target="_blank" title="Tweet">
+								<?php include("img/twitter.svg"); ?>
 								<!-- <img src="img/iconmonstr-twitter-4-icon.svg" alt="Twitter icon" /> -->
 							</a>
 							<a href="https://plus.google.com/share?url=http://aroundme.photos/" target="_blank"  title="Share on Google+">
-								<?php include("img/iconmonstr-google-plus-4-icon.svg"); ?>
+								<?php include("img/google-plus.svg"); ?>
 								<!-- <img src="img/iconmonstr-google-plus-4-icon.svg" alt="Google+ icon" /> -->
 							</a>
 							<a href="https://www.facebook.com/dialog/feed?app_id=587080064742659&display=popup&link=http://aroundme.photos/&redirect_uri=http://aroundme.photos/" target="_blank" title="Share on Facebook">
-								<?php include("img/iconmonstr-facebook-4-icon.svg"); ?>
+								<?php include("img/facebook.svg"); ?>
 								<!-- <img src="img/iconmonstr-facebook-4-icon.svg" alt="Facebook icon" /> -->
 							</a>							
 							<a href="https://github.com/kakauandme/aroundme.photos" target="_blank" title="View on Github">
-								<?php include("img/iconmonstr-github-9-icon.svg"); ?>
+								<?php include("img/github.svg"); ?>
 							<!-- 	<img src="img/iconmonstr-github-9-icon.svg" alt="Github icon" /> -->
 							</a>
 						</p>
@@ -171,9 +114,10 @@ html{height:100%;-webkit-text-size-adjust:100%}body{height:100%;margin:0}#c{widt
 				</span>				
 		      	<span>
 		      		<span id="header">
-		    			<h1>Around me photos</h1>
-		    			<h2>Instagram pictures next to you.</h2>
-		    			<em class="devtext">Website is still in development.<br>Please come back later.</em>
+		    			<h1 itemprop="name"><?php echo $title; ?></h1>
+		    			<h2><?php echo $subtitle; ?>.</h2>
+		    			<p id="description" itemprop="description">Checkout real-time updates around your location <br>and discover what is happening.</p>
+		    			<p id="devtext"><em>More features are coming soon ...</em></p>
 		    		</span>
 		    	</span>		    	
 		    	<span>
--- js/s.js
+++ js/s.js
@@ -344,7 +344,7 @@ function initialize() {
 		}
 
 
-		//getImagesFromInstagram();
+		getImagesFromInstagram();
 
 
 		
