f9f7ea120f2e2fc69954b8750246175100b47c0a
Kirill Kliavin <kirill@studiothick.com>
2014-08-27 23:19:30 +1000

    new FB share

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ html{font-size:125%;line-height:1.7em}body{color:#8c8f91;font-family:L,sans-seri
 <a href="https://plus.google.com/share?url=http://aroundme.photos/" target="_blank" title="Share on Google+">
 <?php include("img/google-plus.svg"); ?>
 </a>
-<a href="https://www.facebook.com/dialog/feed?app_id=587080064742659&display=popup&link=http://aroundme.photos/&redirect_uri=http://aroundme.photos/" target="_blank" title="Share on Facebook">
+<a href="https://www.facebook.com/dialog/share?app_id=587080064742659&display=popup&href=http://aroundme.photos/&redirect_uri=http://aroundme.photos/#_=_" onclick="window.open(this.href, 'mywin', 'left=20,top=20,width=500,height=500,toolbar=0,resizable=0'); return false;" title="Share on Facebook">
 <?php include("img/facebook.svg"); ?>
 </a>
 <?php /*
@@ -107,7 +107,9 @@ Copyright &copy; <?php echo date("Y") ?>
 </div>
 </div>
 <!--[if gt IE 9]><!-->
-<script><?php
+<script>
+if (window.location.hash == '#_=_') window.close();
+<?php
 	echo "var modernBrowser = " . (( preg_match('/bot|crawl|slurp|spider/i', $_SERVER['HTTP_USER_AGENT']))?"true":"false") . ";";   
 ?></script>
 <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAdTpn_GSHnRcfX3vd6jcfibpJMpICcJW4"></script>
