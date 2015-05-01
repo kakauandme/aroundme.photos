e0e33e5214f5e6524cbfb8ea1b92b63c75828c25
Kirill Kliavin <kirill@studiothick.com>
2015-05-01 20:02:59 +1000

    inline magnifier and heart

--- index.php
+++ index.php
@@ -18,47 +18,47 @@ include_once("config.php");
 html{font-size:125%;line-height:1.7em}body{color:#8c8f91;font-family:L,sans-serif}h1,.likes{color:#f74552;font-size:3.4em;font-weight:normal;line-height:1.325em;margin:0.7em 0 0.7em}h2{color:#1a75cf;font-size:1.2em;font-weight:normal;line-height:1.41667em;margin:1em 0 1em}p,em,strong{font-size:1em;font-weight:normal;font-style:normal;line-height:1.7em;margin:0 0 1em}#social p,#social em,#social strong{margin:0}a{color:#fff5a1;text-decoration:none}a:hover{text-decoration:underline}#browserhappy{display:none}.ie #browserhappy,.no-js #browserhappy{display:block;position:absolute;top:0;left:0;width:100%;text-align:right}.ie #browserhappy p,.no-js #browserhappy p{background-color:#4f007d;padding:1em}html{height:100%;-webkit-text-size-adjust:100%}body,#container,#pusher{height:100%;margin:0}.ie body{height:auto;background-color:#24292e}#container{overflow:hidden}#c{position:relative;width:100%;height:100%;display:table;background-color:#24292e}#c>span{display:table-row}#c>span>span{padding:1em;display:table-cell;text-align:right}#pusher{-webkit-transform:translate3d(0, 0, 0);transform:translate3d(0, 0, 0)}#header{vertical-align:middle;height:100%}#footer{vertical-align:bottom}#footer p{margin-bottom:0}#timer{bottom:0;position:absolute;padding:1em;color:#414b56}#timer p{margin-bottom:0}#description{color:#66d43d}#wrap{position:absolute;top:0;left:0;width:100%;height:100%;-webkit-transform:translate3d(100%, 0, 0);transform:translate3d(100%, 0, 0)}#wrap #map{height:100%}#social a,#devtext,#c>span>span#browserhappy{display:none}
 </style>
 <?php /*browsers */ ?>
-<link rel="shortcut icon" href="/img/setup/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/img/config/favicon.ico" type="image/x-icon">
 <?php /*iOS */ ?>
-<link rel="apple-touch-icon" href="/img/setup/apple-touch-icon.png">
+<link rel="apple-touch-icon" href="/img/config/apple-touch-icon.png">
 <?php /* STARTUP IMAGES */ ?>
 <?php /* iPad retina portrait startup image */ ?>
-<link href="/img/setup/iPadRetinaPortrait.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" rel="apple-touch-startup-image">
+<link href="/img/config/iPadRetinaPortrait.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" rel="apple-touch-startup-image">
 <?php /* iPad retina landscape startup image */ ?>
-<link href="/img/setup/iPadRetinaLandscape.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" rel="apple-touch-startup-image">
+<link href="/img/config/iPadRetinaLandscape.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" rel="apple-touch-startup-image">
 <?php /* iPad non-retina portrait startup image */ ?>
-<link href="/img/setup/iPadPortrait.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: portrait)" rel="apple-touch-startup-image">
+<link href="/img/config/iPadPortrait.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: portrait)" rel="apple-touch-startup-image">
 <?php /* iPad non-retina landscape startup image */ ?>
-<link href="/img/setup/iPadRetinaLandscape.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: landscape)" rel="apple-touch-startup-image">
+<link href="/img/config/iPadRetinaLandscape.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: landscape)" rel="apple-touch-startup-image">
 <?php /* iPhone 6 Plus portrait startup image */ ?>
-<link href="/img/setup/iPhone6PlusPortrait.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" rel="apple-touch-startup-image">
+<link href="/img/config/iPhone6PlusPortrait.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" rel="apple-touch-startup-image">
 <?php /* iPhone 6 Plus landscape startup image */ ?>
-<link href="/img/setup/iPhone6PlusLandscape.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" rel="apple-touch-startup-image">
+<link href="/img/config/iPhone6PlusLandscape.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" rel="apple-touch-startup-image">
 <?php /* iPhone 6 startup image */ ?>
-<link href="/img/setup/iPhone6.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+<link href="/img/config/iPhone6.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
 <?php /* iPhone 5 startup image */ ?>
-<link href="/img/setup/iPhone5.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+<link href="/img/config/iPhone5.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
 <?php /* iPhone < 5 retina startup image */ ?>
-<link href="/img/setup/iPhone4.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+<link href="/img/config/iPhone4.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
 <?php /* iPhone < 5 non-retina startup image */ ?>
-<link href="/img/setup/iPhone3.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+<link href="/img/config/iPhone3.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-title" content="<?php echo $siteName; ?>">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <?php /*Android */ ?>
 <meta name="mobile-web-app-capable" content="yes">
-<link rel="icon" type="image/png" href="/img/setup/favicon-196x196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="/img/config/favicon-196x196.png" sizes="196x196">
 <?php /*MS Tiles */ ?>
 <meta name="application-name" content="<?php echo $siteName; ?>"/>
 <meta name="msapplication-config" content="browserconfig.xml" />
 <meta name="msapplication-TileColor" content="#1a75cf">
-<meta name="msapplication-TileImage" content="/img/setup/mstile-144x144.png">
+<meta name="msapplication-TileImage" content="/img/config/mstile-144x144.png">
 <?php /*Twitter cards */ ?>
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:creator" content="@kakauandme">
 <meta name="twitter:title" content="<?php echo $twitterTitle; ?>">
 <meta name="twitter:description" content="<?php echo $description; ?>">
-<meta name="twitter:image:src" content="<?php echo $baseURL; ?>/img/screenshot_sml.png">
+<meta name="twitter:image:src" content="<?php echo $baseURL; ?>/img/config/screenshot_sml.png">
 <?php /*Facebook OG */ ?>
 <meta property="og:title" content="<?php echo $socialTitle; ?>" />
 <meta property="og:site_name" content="<?php echo $siteName; ?>"/>
@@ -66,9 +66,9 @@ html{font-size:125%;line-height:1.7em}body{color:#8c8f91;font-family:L,sans-seri
 <meta property="og:description" content="<?php echo $description; ?>" />
 <meta property="fb:app_id" content="587080064742659" />
 <meta property="og:type" content="website" />
-<meta property="og:image" content="<?php echo $baseURL; ?>/img/screenshot_lrg.png" />
+<meta property="og:image" content="<?php echo $baseURL; ?>/img/config/screenshot_lrg.png" />
 <?php /*Google+ Schema.org microdata */ ?>
-<meta itemprop="image" content="<?php echo $baseURL; ?>/img/screenshot_lrg.png">
+<meta itemprop="image" content="<?php echo $baseURL; ?>/img/config/screenshot_lrg.png">
 </head>
 <body id="body" class="no-js">
 <div id="container">
