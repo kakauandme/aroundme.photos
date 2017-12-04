22c336ca343744d2a89d656899c7755d0a6f88e8
Kirill Kliavin <k.kliavin@gmail.com>
2017-12-05 01:14:03 +1100

    clean up

--- appcache.php
+++ appcache.php
@@ -15,25 +15,8 @@ CACHE:
 
 /favicon.ico
 /apple-touch-icon.png
-/img/config/favicon.ico
-/img/config/apple-touch-icon-57x57.png
-/img/config/apple-touch-icon-60x60.png
-/img/config/apple-touch-icon-72x72.png
-/img/config/apple-touch-icon-76x76.png
-/img/config/apple-touch-icon-114x114.png
-/img/config/apple-touch-icon-120x120.png
-/img/config/apple-touch-icon-144x144.png
-/img/config/apple-touch-icon-152x152.png
-/img/config/apple-touch-icon-180x180.png
 
-/img/config/android-chrome-36x36.png
-/img/config/android-chrome-48x48.png
-/img/config/android-chrome-72x72.png
-/img/config/android-chrome-96x96.png
-/img/config/android-chrome-144x144.png
 /img/config/android-chrome-192x192.png
-/img/config/android-chrome-256x256.png
-img/config/android-chrome-384x384.png
 /img/config/android-chrome-512x512.png
 
 
--- includes/header.php
+++ includes/header.php
@@ -6,33 +6,15 @@
 <?php /*CSS */ ?>
 <style><?php require_once("css/header.css"); ?></style>
 <?php /*browsers */ ?>
-<link rel="shortcut icon" href="/img/config/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 <?php /* iOS meta */?>
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-title" content="<?php echo $shortName; ?>">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <?php /*iOS */ ?>
-<link rel="apple-touch-icon" sizes="57x57" href="/img/config/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/img/config/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/img/config/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/img/config/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/img/config/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/img/config/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/img/config/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/img/config/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/img/config/apple-touch-icon-180x180.png">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="mask-icon" href="/img/config/logo.svg" color="#f74552">
-<?php /* STARTUP IMAGES */ ?>
-<link href="/img/config/iPadRetinaPortrait.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" rel="apple-touch-startup-image">
-<link href="/img/config/iPadRetinaLandscape.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" rel="apple-touch-startup-image">
-<link href="/img/config/iPadPortrait.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: portrait)" rel="apple-touch-startup-image">
-<link href="/img/config/iPadRetinaLandscape.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: landscape)" rel="apple-touch-startup-image">
-<link href="/img/config/iPhone6PlusPortrait.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" rel="apple-touch-startup-image">
-<link href="/img/config/iPhone6PlusLandscape.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" rel="apple-touch-startup-image">
-<link href="/img/config/iPhone6.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
-<link href="/img/config/iPhone5.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
-<link href="/img/config/iPhone4.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
-<link href="/img/config/iPhone3.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+
 <?php /*Android */ ?>
 <link rel="manifest" href="/manifest.json">
 <meta name="mobile-web-app-capable" content="yes">
